6dbc74de5201c7ee4fbdb23dd9f992066b6b76b7
Phong Yang <phongyang88@hotmail.com>
2020-01-21 04:55:38 -0600

    added CartContext and imported it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import { ProductContext} from "./contexts/ProductContext";
+import {CartContext} from "./contexts/CartContext";
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -17,24 +20,23 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={cart}>
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+				<ProductContext.Provider value={{products, addItem}}>
+					<Route
+						exact
+						path="/"
+						component={Products}
 					/>
-				)}
-			/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</ProductContext.Provider>
+			</CartContext.Provider>
+			
 		</div>
 	);
 }
